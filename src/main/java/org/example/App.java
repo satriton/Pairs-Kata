0e1f32e0e95c2ceb410b0eb66fa2374f14c6efd4
Paul ANACLET <panaclet@ippon.fr>
2024-01-23 22:38:38 +0100

    solution

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,27 @@
 package org.example;
 
-public class App {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
+public class App {
+    public static int solver(List<Integer> numbers) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : numbers) {
+            int complement = 10 - num;
+
+            int complementsInMap = countMap.getOrDefault(complement, 0);
+
+            if (complementsInMap > 0) {
+                count++;
+                countMap.put(complement, complementsInMap - 1);
+            } else {
+                countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        return count;
+    }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,11 +7,35 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
-    public void test() {
-        assertTrue(true);
+    public void test_empty_list() {
+        List<Integer> numbers = Collections.emptyList();
+        assertEquals(0, App.solver(numbers));
+    }
+
+    @Test
+    public void test_no_pairs_in_list() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        assertEquals(0, App.solver(numbers));
+    }
+
+    @Test
+    public void test_with_one_pair() {
+        List<Integer> numbers = Arrays.asList(1, 9, 8, 3);
+        assertEquals(1, App.solver(numbers));
+    }
+
+    @Test
+    public void test_with_multiple_pairs() {
+        List<Integer> numbers = Arrays.asList(2, 8, 3, 1, 7, 6, 5, 4, 9, 1, 6, 4, 3, 2, 5, 7, 8, 9);
+        assertEquals(9, App.solver(numbers));
+    }
+
+    @Test
+    public void test_with_same_pairs() {
+        List<Integer> numbers = Arrays.asList(1, 9, 9, 9, 1, 1, 3);
+        assertEquals(3, App.solver(numbers));
     }
 }
